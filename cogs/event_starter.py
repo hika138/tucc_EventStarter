2965157d85ac3401e031b1262eb56c0088a66111
hika138 <hikax138@gmail.com>
2025-09-09 16:44:30 +0900

    init: 初期設定ファイルとREADMEを追加し、イベントスターターコグを実装

---
+++ cogs/event_starter.py
@@ -0,0 +1,63 @@
+import discord
+import os
+import sys
+from typing import Literal
+from discord.ext import commands, tasks
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# 環境変数の取得
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
+guild_id = int(os.environ.get("GUILD_ID"))
+
+# コマンドの本体
+class Starter(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.guild = None
+        self.nearest_event = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # tokenやidの取得
+        self.guild = self.bot.get_guild(guild_id)
+        await self.bot.tree.sync(guild=self.guild)
+    
+    # イベントの変更を検知して一番近いイベントを取得
+    async def get_nearest_event(self) -> discord.ScheduledEvent | None:
+        events = await self.guild.fetch_scheduled_events()
+        if not events:
+            return None
+        events = sorted(events, key=lambda x: x.start_time)
+        return events[0]
+    
+    @commands.Cog.listener()
+    async def on_scheduled_event_create(self, event: discord.ScheduledEvent):
+        self.nearest_event = await self.get_nearest_event()
+        
+    @commands.Cog.listener()
+    async def on_scheduled_event_update(self, event: discord.ScheduledEvent, after: discord.ScheduledEvent):
+        self.nearest_event = await self.get_nearest_event()
+
+    @commands.Cog.listener()
+    async def on_scheduled_event_delete(self, event: discord.ScheduledEvent):
+        self.nearest_event = await self.get_nearest_event()
+    
+    # 最も近いイベントが開始時間になったら開始して通知
+    @tasks.loop(minutes=1)
+    async def check_event_start(self):
+        if not self.nearest_event:
+            return
+        if self.nearest_event.start_time <= discord.utils.utcnow():
+            channel = self.guild.system_channel
+            if channel:
+                await self.nearest_event.start()
+                await channel.send(f"イベント `{self.nearest_event.name}` が開始されました！")
+            self.nearest_event = await self.get_nearest_event()
+    
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(
+        Starter(bot)
+    )
---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+import os
+import sys
+from discord.ext import commands
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+class EventStarter(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            intents=intents,
+            help_command=None,
+            command_prefix='h!'
+        )
+        self.initial_extensions = [
+            "cogs.event_starter"
+        ]
+    
+    async def setup_hook(self):
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
+    
+    async def on_ready(self):
+        print("get on ready!")
+        sys.stdout.flush()
+        return
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+token = os.environ.get("TOKEN") 
+guild_id = int(os.environ.get("GUILD_ID"))
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guild_scheduled_events = True
+
+bot = EventStarter()
+bot.run(token)
